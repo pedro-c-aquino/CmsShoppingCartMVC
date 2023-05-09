4c53e6cc65ef03460e1503ac56138e76e09a7bd8
Pedro Aquino <pcardosodeaquino@gmail.com>
2023-05-09 15:54:05 -0300

    Adicionar arquivos de projeto.

---
+++ CmsShoppingCartMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CmsShoppingCartMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
